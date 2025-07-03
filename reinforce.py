e73d282cfaf55177c349eb931e81966f77547ea2
midorigo <isaac.wold@gmail.com>
2025-07-03 01:50:50 -0600

    accept command line arguments with argparse

--- reinforce.py
+++ reinforce.py
@@ -1,23 +1,26 @@
+import argparse
 import os
 import re
 from pydub import AudioSegment
 
-#User variables
-input_path = "input/Selected Notes.txt"
-output_path = "output/Selected Notes.mp3"
-media_path = "/home/wold/Anki/Anki2/User 1/collection.media/"
-silence_interval_ms = 3000
+parser = argparse.ArgumentParser(prog='reinforce', description='Concatenate audio files from Anki cards')
+parser.add_argument('filename', default='input/Selected Notes.txt', help='File to operate on, e.g. \"input/Selected Notes.txt\"')
+parser.add_argument('-b', '--base', default=os.environ.get("HOME")+'/.local/share/Anki2/User 1/collection.media', help='Path of collection.media directory; default=\"~/.local/share/Anki2/User 1/collection.media/\"')
+parser.add_argument('-s', '--silence', default=3000, type=int, help='Length of silence to insert between audio clips in ms; default=3000')
+args = parser.parse_args()
 
-with open(input_path, 'r', encoding='utf-8') as input:
+output_path = "output/Selected Notes.mp3"
+
+with open(args.filename, 'r', encoding='utf-8') as input:
     audio_files = []
     for line in input:
         audio_files.extend(re.findall(r"\[sound:(.*?)\]", line))
     concatenated = AudioSegment.empty()
-    silence = AudioSegment.silent(duration=silence_interval_ms)
+    silence = AudioSegment.silent(duration=args.silence)
     for filename in audio_files:
-        file_path = media_path + filename
-        print(file_path)
+        file_path = args.base + filename
         audio = AudioSegment.from_file(file_path, format="mp3") + silence
         concatenated += audio
+        print(file_path)
     concatenated.export(output_path, format="mp3")
-    print(f"File written to {output_path}") 
+    print(f"File written to {os.getcwd()}/{output_path}") 
